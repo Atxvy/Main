8b1452ce7d585e0b18b5213e75b1c9242925ecab
Atxvy <automacrohub@gmail.com>
2026-09-22 09:23:27 -0700

    Update Jailed.lua

--- Trials/Free/Jailed.lua
+++ Trials/Free/Jailed.lua
@@ -1,5 +1,4 @@
 
- loadstring(game:HttpGet("https://raw.githubusercontent.com/avtryxz/autogutlin/refs/heads/main/autogutlin.lua"))()
 TDS:Loadout("Assassin", "Scout", "Paintballer", "DJ Booth", "Gatling Gun")
 TDS:Mode("Trial")
 TDS:Place("Scout", 135.47198486328125, 2.0749998092651367, -47.1058349609375)
