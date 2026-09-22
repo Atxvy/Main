ccdf817772c9d858fd8b676a07b2c53cc7f92359
Atxvy <automacrohub@gmail.com>
2026-09-22 09:22:16 -0700

    Update Fog.lua

--- Trials/Premium/Fog.lua
+++ Trials/Premium/Fog.lua
@@ -1,7 +1,6 @@
 local Globals = getgenv()
 Globals.AutoMercenary = true
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/avtryxz/autogutlin/refs/heads/main/autogutlin.lua"))()
 TDS:Mode("Trial")
 
 TDS:Place("Trapper", -6.890721321105957, 0.9299085736274719, 33.54568099975586,  true)
